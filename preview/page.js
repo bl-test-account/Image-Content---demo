4240443f08edaf5c89abe95114d64262cbc18e1c
bl-test-account <plugin.developer@bytelight.com>
2014-05-29 07:37:37 -0700

    Released on: 2014-05-29 10:37:36 -0400

---
+++ preview/page.js
@@ -0,0 +1,24 @@
+/*
+Get the current location and user information by calling
+BL.getContext.The returned object contains the lightGroupId,
+lightGroupName, and userId properties.
+*/
+context = BL.getContext();
+ /*
+Retrieve static content with Bl.getStaticContent.
+Static content is stored as an object with properties
+equal to the identifiers of each piece of content. Each property
+contains an array with the content corresponding
+to each update of the app.
+*/
+var savedContent = BL.getStaticContent(APP.appId);
+var src;
+if (BL.isTablet()) {
+  src = '<img width="768px" src="' + savedContent.ipad_image_url[0] + '">';
+}
+else {
+  src = '<img src="' + savedContent.iphone_image_url[0] + '">';
+}
+$('.content-image').append(src)
+//console.log($('.content-image img').attr('src'))
+
